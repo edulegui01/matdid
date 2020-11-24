07103c51ccbf9bfcc3827a1a5e9800a7cc6fe04d
Santiago <edulegui01@gmail.com>
2020-11-23 21:13:41 -0300

    Updates

---
+++ precache-manifest.070b47de7c01d0e876dece60449538a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b6e5c4224d2298869c988408ba78ac",
+    "url": "/matdid/index.html"
+  },
+  {
+    "revision": "951349b6b6373fe52b91",
+    "url": "/matdid/static/css/main.db22e019.chunk.css"
+  },
+  {
+    "revision": "7b95ca8016058ea78093",
+    "url": "/matdid/static/js/2.d97bf249.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/matdid/static/js/2.d97bf249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951349b6b6373fe52b91",
+    "url": "/matdid/static/js/main.1e6299b9.chunk.js"
+  },
+  {
+    "revision": "ff0d830546cca69e994a",
+    "url": "/matdid/static/js/runtime-main.34150561.js"
+  },
+  {
+    "revision": "6c1655862462a337ff95bc0e87d0767d",
+    "url": "/matdid/static/media/logo.6c165586.png"
+  }
+]);
